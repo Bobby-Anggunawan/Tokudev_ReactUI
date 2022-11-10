fbed1f6df02ceb391d92a5af5ebfba90eb34f468
Bobby Anggunawan <yeremia.blog@gmail.com>
2022-11-10 15:39:25 +0700

    table of content selesai

--- src/myLib/page_builder.js
+++ src/myLib/page_builder.js
@@ -5,6 +5,10 @@ import Typography from '@mui/material/Typography';
 import HeadingToku from '../component/heading';
 import ImageToku from '../component/image';
 import PostHeaderToku from '../component/post_header';
+import Box from '@mui/material/Box';
+import { tableOfContentsWidth, muiToolbarHeight, contentHorizontalPadding, contentVerticalPadding, borderRadius} from '../constant';
+import Link from '@mui/material/Link';
+import ScrollSpy from 'react-scrollspy-navigation';
 
 function PageBuilder(props) {
 
@@ -40,6 +44,7 @@ function PageBuilder(props) {
     //=======================
     var prev_arg = [];
     var hasil = [];
+    var scrollSpyContent = [];
     var counter  = 0;
     while(counter<data.length){
         if(prev_arg.length == 0){
@@ -69,30 +74,47 @@ function PageBuilder(props) {
             }
             else if(prev_arg[0] == "h1" || prev_arg[0] == "h2" || prev_arg[0] == "h3" || prev_arg[0] == "h4"){
                 var hasil2 = [];
-                
+                var headingName = "";
+
                 if(prev_arg[0] == "h1"){
                     prev_arg = [];
                     hasil2.push(
                         <HeadingToku variant={1} title={data[counter]} key={counter}/>
                     );
+                    headingName = data[counter];
+                    scrollSpyContent.push(
+                        <Link href={`#${data[counter]}`} ref={React.createRef()}>{data[counter]}</Link>
+                    )
                 }
                 else if(prev_arg[0] == "h2"){
                     prev_arg = [];
                     hasil2.push(
                         <HeadingToku variant={2} title={data[counter]} key={counter}/>
                     );
+                    headingName = data[counter];
+                    scrollSpyContent.push(
+                        <Link href={`#${data[counter]}`} ref={React.createRef()}>{data[counter]}</Link>
+                    )
                 }
                 else if(prev_arg[0] == "h3"){
                     prev_arg = [];
                     hasil2.push(
                         <HeadingToku variant={3} title={data[counter]} key={counter}/>
                     );
+                    headingName = data[counter];
+                    scrollSpyContent.push(
+                        <Link href={`#${data[counter]}`} ref={React.createRef()}>{data[counter]}</Link>
+                    )
                 }
                 else if(prev_arg[0] == "h4"){
                     prev_arg = [];
                     hasil2.push(
                         <HeadingToku variant={4} title={data[counter]} key={counter}/>
                     );
+                    headingName = data[counter];
+                    scrollSpyContent.push(
+                        <Link href={`#${data[counter]}`} ref={React.createRef()}>{data[counter]}</Link>
+                    )
                 }
                 counter+=1;
                 while(true){
@@ -127,7 +149,7 @@ function PageBuilder(props) {
                     counter+=1;
                 }
                 hasil.push(
-                    <section key={counter}>{hasil2}</section>
+                    <section key={counter} id={headingName}>{hasil2}</section>
                 );
             }
         }
@@ -137,7 +159,39 @@ function PageBuilder(props) {
     return (
         <React.Fragment>
             <PostHeaderToku title={title} date={updateDate} imgAlt={title} imgUrl={poster}/>
-            {hasil}
+            <Box sx={{display: "flex", marginTop: contentVerticalPadding}}>
+                {/*Blog Post*/}
+                <Box sx={{flexGrow: 1}}>
+                    {hasil}
+                </Box>
+
+
+                {/*Table of contents*/}
+                <Box sx={{
+                    width: tableOfContentsWidth,
+                    flexShrink: 0,
+                    '& .active':{
+                        color: "error.main"
+                    },
+                    '& a': {
+                        display: "block",
+                        borderRadius: borderRadius,
+                        px: 1.5,
+                        textDecoration: "none"
+                    },
+                    '& a:hover': {
+                        backgroundColor: 'primary.main',
+                        color: "primary.contrastText"
+                    }
+                }}>
+                    <section style={{position: "sticky", top: `${parseInt(muiToolbarHeight)+parseInt(contentVerticalPadding)}px`, textAlign: "center", paddingLeft: contentHorizontalPadding}}>
+                        <Typography component="h2" variant="h5">Table of contents</Typography>
+                        <ScrollSpy>
+                            {scrollSpyContent}
+                        </ScrollSpy>
+                    </section>
+                </Box>
+            </Box>
         </React.Fragment>
     );
 }
