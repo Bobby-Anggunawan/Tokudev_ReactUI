5a9fa0edc2e2ac6df83c0cffb92fbe4ee9479946
Bobby Anggunawan <yeremia.blog@gmail.com>
2022-11-21 18:49:59 +0700

    sudah ada halaman loading dan halaman not found di post yng tidak ada

--- src/myLib/page_builder.js
+++ src/myLib/page_builder.js
@@ -6,42 +6,68 @@ import HeadingToku from '../component/heading';
 import ImageToku from '../component/image';
 import PostHeaderToku from '../component/post_header';
 import Box from '@mui/material/Box';
-import { tableOfContentsWidth, muiToolbarHeight, contentHorizontalPadding, contentVerticalPadding, borderRadius, db} from '../constant';
+import { tableOfContentsWidth, muiToolbarHeight, contentHorizontalPadding, contentVerticalPadding, borderRadius, db } from '../constant';
 import Link from '@mui/material/Link';
 import ScrollSpy from 'react-scrollspy-navigation';
 import PageBuilderFunction from './pageBuilderFunction';
+import LoadingPage from '../pages/Loading';
+import NotFound from '../pages/NotFound';
 
 function PageBuilder(props) {
 
-    const [data, setData] = React.useState([]);
-    const [title, setTitle] = React.useState("Loading");
-    const [updateDate, setUpdateDate] = React.useState("Loading");
-    const [poster, setPostere] = React.useState("Loading");
+    const [isLoading, setLoading] = React.useState(true);
+
+    const [data, setData] = React.useState(null);
+    const [title, setTitle] = React.useState(null);
+    const [updateDate, setUpdateDate] = React.useState(null);
+    const [poster, setPostere] = React.useState(null);
     React.useEffect(() => {
         const docRef = doc(db, "TutorialPost", props.pageUrl);
         const docSnap = getDoc(docRef).then((doc) => {
-            setData(doc.data().content);
-            setTitle(doc.data().title);
-            setPostere(doc.data().poster);
+            if (doc.exists()) {
+                setData(doc.data().content);
+                setTitle(doc.data().title);
+                setPostere(doc.data().poster);
 
-            var t = new Date(Date.UTC(1970, 0, 1)); // Epoch
-            t.setUTCSeconds(doc.data().date.seconds);
-            setUpdateDate(t.toDateString());
+                var t = new Date(Date.UTC(1970, 0, 1)); // Epoch
+                t.setUTCSeconds(doc.data().date.seconds);
+                setUpdateDate(t.toDateString());
+            }
+
+            setLoading(false);
         });
     }, []);
 
     //=======================
-    
-    var pageResult = PageBuilderFunction(data)
-    var hasil = pageResult.Hasil;
-    var scrollSpyContent = pageResult.ScrollSpyContent;
+
+    var pageResult = null;
+    var hasil = null;
+    var scrollSpyContent = null;
+    if(data!=null){
+        pageResult = PageBuilderFunction(data);
+        hasil = pageResult.Hasil;
+        scrollSpyContent = pageResult.ScrollSpyContent;
+    }
+
+    if (isLoading) {
+        return (
+            <LoadingPage />
+        );
+    }
+    else {
+        if(title==null){
+            return(
+                <NotFound/>
+            );
+        }
+    }
 
     return (
         <React.Fragment>
-            <PostHeaderToku title={title} date={updateDate} imgAlt={title} imgUrl={poster}/>
-            <Box sx={{display: "flex", marginTop: contentVerticalPadding}}>
+            <PostHeaderToku title={title} date={updateDate} imgAlt={title} imgUrl={poster} />
+            <Box sx={{ display: "flex", marginTop: contentVerticalPadding }}>
                 {/*Blog Post*/}
-                <Box sx={{flexGrow: 1}}>
+                <Box sx={{ flexGrow: 1 }}>
                     {hasil}
                 </Box>
 
@@ -50,7 +76,7 @@ function PageBuilder(props) {
                 <Box sx={{
                     width: tableOfContentsWidth,
                     flexShrink: 0,
-                    '& .active':{
+                    '& .active': {
                         color: "error.main"
                     },
                     '& a': {
@@ -64,7 +90,7 @@ function PageBuilder(props) {
                         color: "primary.contrastText"
                     }
                 }}>
-                    <section style={{position: "sticky", top: `${parseInt(muiToolbarHeight)+parseInt(contentVerticalPadding)}px`, textAlign: "center", paddingLeft: contentHorizontalPadding}}>
+                    <section style={{ position: "sticky", top: `${parseInt(muiToolbarHeight) + parseInt(contentVerticalPadding)}px`, textAlign: "center", paddingLeft: contentHorizontalPadding }}>
                         <Typography component="h2" variant="h5">Table of contents</Typography>
                         <ScrollSpy>
                             {scrollSpyContent}
---
+++ src/pages/Loading.js
@@ -0,0 +1,10 @@
+import { Box } from '@mui/material'
+import React from 'react'
+
+export default function LoadingPage() {
+  return (
+    <Box>
+        <h1>Loading...</h1>
+    </Box>
+  )
+}
